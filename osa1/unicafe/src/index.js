f18c848ec26dde1d9004426b3a52162eb7d45115
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-05-24 12:19:54 +0300

    1.6 - unicafe step 1 toimii

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+const Display = ({ label, value }) => (
+  <div>{label}: {value}</div>
+)
+
+const Button = ({ text, handleClick }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const App = () => {
+  const [ feedbackGood, setFeedbackGood ] = useState(0)
+  const [ feedbackNeutral, setFeedbackNeutral ] = useState(0)
+  const [ feedbackBad, setFeedbackBad ] = useState(0)
+
+  const addFeedback = (counter, setCounter) => setCounter(counter + 1)
+
+  return (
+    <div>
+      <div>
+        <h1>Feedback</h1>
+        <Button text="Great!" handleClick={() => addFeedback(feedbackGood, setFeedbackGood)} />
+        <Button text="meh" handleClick={() => addFeedback(feedbackNeutral, setFeedbackNeutral)} />
+        <Button text="Hated it :(" handleClick={() => addFeedback(feedbackBad, setFeedbackBad)} />
+      </div>
+      <div>
+        <Display label="Good feedback" value={feedbackGood} />
+        <Display label="Neutral feedback" value={feedbackNeutral} />
+        <Display label="Bad feedback" value={feedbackBad} />
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <App />,
