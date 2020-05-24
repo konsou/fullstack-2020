9c40a1dc5f8797ea21c2a85aea9b6003854ccc40
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-05-24 11:59:12 +0300

    Osa 1d - handleClick-tehdasfunktio

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,25 +1,50 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Display = ({ counter }) => ( <div>Counter: {counter}</div> )
+const Display = ({ title, counter }) => ( <div>{title}: {counter}</div> )
+
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0){ 
+    return (
+      <div>
+        Use the app by clicking the buttons
+      </div>
+    )
+  } else { 
+    return (
+      <div>
+        Button click history: { allClicks.join(" ") }
+      </div>
+    )
+  }
+
+}
 const Button = ({ text, handleClick }) => ( <button onClick={handleClick}>{text}</button> )
-  
+
+
+{/* NYT OSA 1d */}
 
 const App = props => {
   console.log("Apissa ollaan")
 
-  const [ counter, setCounter ] = useState(0)
+  const [ leftClicks, setLeftClicks ] = useState(0)
+  const [ rightClicks, setRightClicks ] = useState(0)
+  const [ allClicks, setAllClicks ] = useState([])
 
-  const increaseCounter = () => setCounter(counter + 1)
-  const decreaseCounter = () => setCounter(counter - 1)
-  const resetCounter = () => setCounter(0)
+  const handleClick = (counter, setCounter, stringIdentifier, historyArray, setHistoryArray) => {
+    return () => {
+      setCounter(counter + 1)
+      setHistoryArray(historyArray.concat(stringIdentifier))
+    }
+  }
 
   return (
   <div>
-    <Display counter={counter} />
-    <Button text="+1" handleClick={increaseCounter} />
-    <Button text="-1" handleClick={decreaseCounter} />
-    <Button text="Reset" handleClick={resetCounter} />
+    <Display title="Left" counter={leftClicks} />
+    <Display title="Right" counter={rightClicks} />
+    <History allClicks={allClicks} />
+    <Button text="Left" handleClick={handleClick(leftClicks, setLeftClicks, "L", allClicks, setAllClicks)} />
+    <Button text="Right" handleClick={handleClick(rightClicks, setRightClicks, "R", allClicks, setAllClicks)} />
   </div>
   )
 }
