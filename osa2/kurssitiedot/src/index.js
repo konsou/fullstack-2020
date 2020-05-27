6ea3a7384b843f8ae1c2fff0eb0da56da2ac75bd
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-05-27 17:19:40 +0300

    2.5: Course-komponentti (ja sen alikomponentit) omaan moduuliinsa

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,57 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-
-const Course = ({ course }) => {
-  console.log("Course - course: ", course)
-  return (
-    <div>
-      <Header text={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />    
-    </div>
-  )
-}
-
-
-const Header = ({ text }) => (
-  <h1>{text}</h1>
-)
-
-
-const Content = ({ parts }) => {
-  console.log('in Content - parts: ', parts)
-  return (
-    <ul>
-      {parts.map(part => 
-        <Part key={part.id} part={part} />
-      )}
-    </ul>
-  )
-}
-
-
-const Part = ({ part }) => {
-  console.log('in Part - part: ', part)
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-
-const Total = ({ parts }) => (
-  <p>Number of exercises: {sumProperties(parts, "exercises")}</p>
-)
-
-
-const sumProperties = (object, propertyName) => {
-  return object.reduce(
-    (accumulator, object) => accumulator += object[propertyName], 
-    0 // counter starting value
-  )
-}
+import Course from './components/Course'
 
 
 const App = () => {
