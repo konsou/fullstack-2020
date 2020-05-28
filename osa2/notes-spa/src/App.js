a1c413e1380db2440953f48026f071da14b345f8
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-05-28 20:59:17 +0300

    Osa 2b - muistiinpanot - uuden muistiinpanon lisääminen lomakkeella

--- osa2/notes-spa/src/App.js
+++ osa2/notes-spa/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
 
-const App = ({ notes }) => {
-    console.log('App - notes: ', notes)
+const App = (props) => {
+    console.log('App - notes: ', props.notes)
+
+    const [ notes, setNotes ] = useState(props.notes)
+    const [ newNote, setNewNote ] = useState('new note text')
+
+    const addNote = (event) => {
+      event.preventDefault()
+      console.log('button clicked', event.target)
+      console.log(newNote)
+
+      const noteObject = {
+        content: newNote,
+        date: new Date().toISOString(),
+        important: Math.random() > 0.5,
+        id: notes.length + 1,
+      }
+
+      setNotes(notes.concat(noteObject))
+      setNewNote('')
+    }
+
+    const handleNoteChange = (event) => {
+      // console.log(event.target.value)
+      setNewNote(event.target.value)
+    }
   
     return (
       <div>
@@ -13,6 +37,13 @@ const App = ({ notes }) => {
             <Note key={note.id} note={note} />
           )}
         </ul>
+        <form onSubmit={addNote}>
+          <input 
+            value={newNote} 
+            onChange={handleNoteChange}
+            />
+          <button type="submit">Save</button>
+        </form>
       </div>
     )
   }
