f511185143d2c089e86bdc9a7c36026b9d427b28
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-05-31 15:56:03 +0300

    2.6: puhelinluettelo step1 - nyt voi lisätä nimiä

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Person from './components/Person'
 
 const App = () => {
   const [ persons, setPersons] = useState([
@@ -6,19 +7,33 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleNewName = (event) => {
+      event.preventDefault()
+      console.log(newName)
+      setPersons(persons.concat({ name: newName }))
+      setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+      //console.log(event.target.value)
+      setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input onChange={handleNameChange} value={newName} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={handleNewName} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        { persons.map(person => <li key={person.name}><Person person={person} /></li>) }
+      </ul>
     </div>
   )
 
---
+++ osa2/puhelinluettelo/src/components/Person.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Person = ({ person }) => {
+    console.log(person)
+    return (
+        person.name
+    )
+}
+
+export default Person
