639a4ba41f327efbc5ff9936f2cfed2a6a4b8d2d
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-05-31 16:13:58 +0300

    2.8: puhelinluettelo step - herjaa duplikaattinimistä

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,40 +2,53 @@ import React, { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+    const [ persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ]) 
+    const [ newName, setNewName ] = useState('')
+    const [ errorMessage, setErrorMessage] = useState('')
 
-  const handleNewName = (event) => {
-      event.preventDefault()
-      console.log(newName)
-      setPersons(persons.concat({ name: newName }))
-      setNewName('')
-  }
+    const handleNewName = (event) => {
+        event.preventDefault()
+        //console.log(newName)
+        const newPerson = { name: newName }
+        console.log('new person: ', newPerson)
+        // Check for duplicate entries
+        if (persons.some(person => person.name === newPerson.name)) { 
+            console.log('person exists')
+            alert(`${newName} already exists!`)
+            //setErrorMessage('{person.name} already exists!') 
+            }
+        else {
+            console.log('person doesn\'t exist, adding')
+            setPersons(persons.concat(newPerson))
+            setNewName('')
+        }
+    }
 
-  const handleNameChange = (event) => {
-      //console.log(event.target.value)
-      setNewName(event.target.value)
-  }
+    const handleNameChange = (event) => {
+        //console.log(event.target.value)
+        setNewName(event.target.value)
+    }
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
+    return (
         <div>
-          name: <input onChange={handleNameChange} value={newName} />
+        <h2>Phonebook</h2>
+        <form>
+            <div id="error">{errorMessage}</div>
+            <div>
+            name: <input onChange={handleNameChange} value={newName} />
+            </div>
+            <div>
+            <button onClick={handleNewName} type="submit">add</button>
+            </div>
+        </form>
+        <h2>Numbers</h2>
+        <ul>
+            { persons.map(person => <li key={person.name}><Person person={person} /></li>) }
+        </ul>
         </div>
-        <div>
-          <button onClick={handleNewName} type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        { persons.map(person => <li key={person.name}><Person person={person} /></li>) }
-      </ul>
-    </div>
-  )
+    )
 
 }
 
--- osa2/puhelinluettelo/src/components/Person.js
+++ osa2/puhelinluettelo/src/components/Person.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Person = ({ person }) => {
-    console.log(person)
+    //console.log(person)
     return (
         person.name
     )
