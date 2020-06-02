f30e968f68270a233612e9c3e5fd0c8e756604a2
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-02 19:14:04 +0300

    2.11: puhelinluettelo step6 - alkutila haetaan axiosilla json-serveriltä

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import AddPersonForm from './components/AddPersonForm'
 
 const App = () => {
-    const [ persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]) 
+    const [ persons, setPersons] = useState([]) 
     const [ newName, setNewName ] = useState('')
     const [ newPhoneNumber, setNewPhoneNumber ] = useState('')
     const [ filter, setFilter] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('axios response:')
+                console.log(response)
+                setPersons(response.data)
+            })
+    }, []) // run only once
 
     const handleNewPerson = (event) => {
         event.preventDefault()
