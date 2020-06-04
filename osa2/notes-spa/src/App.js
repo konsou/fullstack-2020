175235cf5f1e39c22d3f392bb05beb2167c9904e
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-04 19:17:44 +0300

    Osa 2 - notes-spa - muistiinpanojen tärkeyttä voi muuttaa

--- osa2/notes-spa/src/App.js
+++ osa2/notes-spa/src/App.js
@@ -45,6 +45,20 @@ const App = (props) => {
       setNewNote(event.target.value)
     }
 
+    const toggleImportanceOf = (id) => {
+      console.log('importance of ' + id + ' needs to be changed')
+      const url = `http://localhost:3001/notes/${id}`
+      const note = notes.find(note => note.id === id)
+      const changedNote = { ...note, important: !note.important }
+      console.log('note after change: ', changedNote)
+
+      axios
+        .put(url, changedNote)
+        .then(response =>{
+          setNotes(notes.map(note => note.id !== id ? note : response.data))
+        })
+    }
+
     const notesToShow = showAll
       ? notes
       : notes.filter(note => note.important)
@@ -59,7 +73,7 @@ const App = (props) => {
         </div>
         <ul>
           {notesToShow.map(note => 
-            <Note key={note.id} note={note} />
+            <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
           )}
         </ul>
         <form onSubmit={addNote}>
