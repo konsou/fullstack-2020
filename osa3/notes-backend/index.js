d39cee31c4a3808780c7c7be4cb6f734831380e2
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-13 19:32:44 +0300

    Osa 3a - 'REST-y' api

---
+++ osa3/notes-backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+let notes = [ { id: 1, content: "HTML is easy - refresh test", date: "2020-01-10T17:30:31.098Z", important: true }, { id: 2, content: "Browser can execute only Javascript", date: "2020-01-10T18:39:34.091Z", important: false }, { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2020-01-10T19:20:14.298Z", important: true }]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const note = notes.find(note => note.id === id)
+    console.log(note)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+
+app.get('/notes', (request, response) => {
+    response.json(notes)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
