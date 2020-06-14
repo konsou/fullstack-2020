b9069113c9f607a5bed11b5b20e335f5baadbb0b
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-06-14 14:50:46 +0300

    OSa 3a - muistiinpanojen luonti ja poisto

--- osa3/notes-backend/index.js
+++ osa3/notes-backend/index.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const app = express()
 
-let notes = [ { id: 1, content: "HTML is easy - refresh test", date: "2020-01-10T17:30:31.098Z", important: true }, { id: 2, content: "Browser can execute only Javascript", date: "2020-01-10T18:39:34.091Z", important: false }, { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2020-01-10T19:20:14.298Z", important: true }]
+app.use(express.json())
+
+let notes = [ 
+    { 
+        id: 1, 
+        content: "HTML is easy - refresh test", 
+        date: "2020-01-10T17:30:31.098Z", 
+        important: true 
+    }, 
+    { 
+        id: 2, 
+        content: "Browser can execute only Javascript", 
+        date: "2020-01-10T18:39:34.091Z", 
+        important: false 
+    }, 
+    { 
+        id: 3, 
+        content: "GET and POST are the most important methods of HTTP protocol", 
+        date: "2020-01-10T19:20:14.298Z", 
+        important: true 
+    }
+]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
@@ -20,8 +41,42 @@ app.get('/api/notes/:id', (request, response) => {
     }
 })
 
+const generateId = () => {
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(note => note.id))
+    : 0
+    return maxId + 1
+}
 
-app.get('/notes', (request, response) => {
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'Missing content'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date().toISOString(),
+        id: generateId()
+    }
+
+    console.log(note)
+
+    notes = notes.concat(note)
+    response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
